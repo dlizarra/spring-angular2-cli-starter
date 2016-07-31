43f1b4c3408a5247c5ad846d27e799f23c176b9a
David Lizárraga <dlizarra5@gmail.com>
2016-07-31 14:11:42 +0200

    Updated tests configurations to use some of  the new Spring Boot 1.4 test annotations.

--- src/test/java/com/dlizarra/starter/support/AbstractWebIntegrationTest.java
+++ src/test/java/com/dlizarra/starter/support/AbstractWebIntegrationTest.java
@@ -1,16 +1,13 @@
 package com.dlizarra.starter.support;
 
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.boot.test.WebIntegrationTest;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
 import com.dlizarra.starter.AppConfig;
 import com.dlizarra.starter.DatabaseConfig;
 import com.dlizarra.starter.SecurityConfig;
 import com.dlizarra.starter.StarterProfiles;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.RANDOM_PORT,
